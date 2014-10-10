947166a36f8ce9fecaf017675642399eee9a7109
Josh Kaplan <joshbrentkaplan@gmail.com>
2014-10-09 21:03:46 -0500

    haha uh

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ GainesStreetFest::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += %w( whole_site.css )
+  config.assets.precompile += %w( whole_site.css.scss )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
